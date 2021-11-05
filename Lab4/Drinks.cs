29392cd433bbfd12982ffc0da4e8f33e973b1f02
Денис Афанасьев <hodache@yandex.ru>
2021-11-05 19:40:11 +0800

    Добавьте файлы проекта.

---
+++ Lab4/Drinks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Drink { }
+
+    public enum JuiceType { apple, orange, cherry };
+
+    public class Juice : Drink
+    {
+        public JuiceType type = JuiceType.apple;
+        public bool withPulp = false;
+    }
+
+    public enum SodaType { coke, fanta, sprite };
+
+    public class Soda : Drink
+    {
+        public SodaType type = SodaType.coke;
+        public int bubblesNumber = 0;
+    }
+
+    public enum AlcoholType {beer, wine, cognac};
+
+    public class Alcohol : Drink
+    {
+        public int strength = 0;
+        public AlcoholType type = AlcoholType.beer;
+    }
+}
